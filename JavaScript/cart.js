e14fb7f44a1e09c0e8169696e98f9e01eb4800be
Lyes <lyesmedjahed17@gmail.com>
2025-11-15 14:19:37 +0100

    correction of js links

--- JavaScript/cart.js
+++ JavaScript/cart.js
@@ -1,5 +1,5 @@
-import { initPaymentPopup } from "./paymentPopup.js";
-import { calculation } from "./cartCounter.js";
+import { initPaymentPopup } from "https://lyes-fedev.github.io//paymentPopup.js";
+import { calculation } from "https://lyes-fedev.github.io//cartCounter.js";
 
 document.addEventListener('DOMContentLoaded', () => {
   const cartContainer = document.getElementById('cart-container');
